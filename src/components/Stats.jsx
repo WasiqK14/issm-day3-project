e1339cd0bfa547381c9dc0e4517c0b7783b31af6
Wasiqk14 <wasiq1432000@gmail.com>
2022-10-22 20:02:33 +0500

    visual:fixed issues regarding all screens reponsiveness with stats section on home

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -3,26 +3,26 @@ import React from 'react'
 const Stats = () => {
   return (
     <div className='bg-[#DEC7AC] p-28'>
-        <div className='grid grid-cols-4 gap-4 md:grid-cols-4 sm:grid-cols-1'>
-            <div className='flex flex-col justify-center items-center border-r-2 border-black h-[200px]'>
+        <div className='grid lg:grid-cols-4 gap-4 md:grid-cols-1 sm:grid-cols-1'>
+            <div className='flex flex-col justify-center items-center h-[200px] lg:border-r-2 border-black '>
                 <span className='text-white font-sans text-[64px] font-semibold'>
                     1000+
                 </span>
                 <p className='text-white font-sans text-[18px] text-center'>employees throughout the U.S</p>
             </div>
-            <div className='flex flex-col justify-center items-center border-r-2 border-black h-[200px]'>
+            <div className='flex flex-col justify-center items-center h-[200px] lg:border-r-2 border-black '>
                 <span className='text-white font-sans text-[64px] font-semibold'>
                    25
                 </span>
                 <p className='text-white font-sans text-[18px] text-center'>offices in key markets across U.S.</p>
             </div>
-            <div className='flex flex-col justify-center items-center border-r-2 border-black h-[200px]'>
+            <div className='flex flex-col justify-center items-center h-[200px] lg:border-r-2 border-black '>
                 <span className='text-white font-sans text-[64px] font-semibold'>
                     $8.8B
                 </span>
                 <p className='text-white font-sans text-[18px] text-center '>development pipeline</p>
             </div>
-            <div className='flex flex-col justify-center items-centerh-[200px]'>
+            <div className='flex flex-col justify-center items-center h-[200px]'>
                 <span className='text-white font-sans text-[64px] font-semibold '>
                     3500+
                 </span>
